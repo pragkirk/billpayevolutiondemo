4bb33fb0bdab20be1f16cc0b0daa68f3b6a74cc2
pragkirk <pragkirk@kirkk.com>
2016-03-29 19:31:26 -0500

    Spark client and service.
    Refactored Audit to REST service using Spark

---
+++ billpayosgiBlueprint/restaudit/restauditclient/src/com/extensiblejava/restaudit/RestAuditFacade.java
@@ -0,0 +1,52 @@
+package com.extensiblejava.restaudit;
+
+import com.extensiblejava.audit.*;
+import java.math.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RestAuditFacade implements AuditFacade {
+
+	public BigDecimal audit(Auditable auditable) throws AuditException {
+		try {
+		
+			URL url = new URL("http://localhost:4567/audit?amount=" + auditable.getAmount().setScale(2, BigDecimal.ROUND_UP).toString());
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+					+ conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+				(conn.getInputStream())));
+
+			String output = br.readLine();
+			int begLoc = output.indexOf(":");
+			int endLoc = output.indexOf("}");
+			String newString = output.substring(begLoc+2,endLoc-1);
+			
+			//System.out.println(output);
+			//System.out.println("---- " + newString + "----");
+
+			conn.disconnect();
+			
+			return new BigDecimal(newString);
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			throw new AuditException();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new AuditException();
+		}
+
+	}
+}
---
+++ billpayosgiBlueprint/restaudit/restauditservice/src/main/java/com/extensiblejava/audit/restservice/Auditor.java
@@ -0,0 +1,34 @@
+package com.kirkk.extensiblejava.audit.restservice;
+
+import static spark.Spark.*;
+import com.google.gson.Gson;
+import com.extensiblejava.audit.audit2.AuditFacade2;
+import com.extensiblejava.audit.Auditable;
+import static com.kirkk.extensiblejava.audit.restservice.JsonUtil.*;
+import java.math.*;
+ 
+public class Auditor {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello, World!");
+        get("/audit", "application/json", (req, res) -> {
+        	final String amount = req.queryParams("amount");
+        	
+        	AuditFacade2 auditor = new AuditFacade2();
+        	BigDecimal auditedAmt = auditor.audit(new Auditable() {
+        		public BigDecimal getAmount() { return new BigDecimal(amount); }
+        	});
+        	
+        	return new Payment().setPayment(auditedAmt.setScale(2, BigDecimal.ROUND_UP).toString());
+        	
+        }, json());
+        /*get("/audit", "application/json", (req, res) -> {
+        	String principle = req.queryParams("amount");
+        	
+        	//return new Payment().setPayment(new LoanCalculator().calculatePayment(principle, rate, term));
+        }, json());*/
+        
+        /*get("/loan", (req, res) -> {        	
+        	return new Payment().setPayment(new LoanCalculator().calculatePayment("15000","6.0","60"));
+        }, json());*/
+    }
+}
